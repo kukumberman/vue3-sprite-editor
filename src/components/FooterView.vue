375173c33382c8203d723e54c08264564ad5d63d
Dima <strongbaby1337@gmail.com>
2022-04-13 17:23:47 +0300

    Create FooterView.vue

---
+++ src/components/FooterView.vue
@@ -0,0 +1,18 @@
+<template>
+  <a :href="link" target="_blank">{{ link }}</a>
+</template>
+
+<script>
+export default {
+  props: {
+    link: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
